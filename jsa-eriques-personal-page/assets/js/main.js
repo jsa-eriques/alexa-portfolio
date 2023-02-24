ae89fc2ac1506522fbfda28a319a799b8aaab151
artsu <jsa.eric@hotmail.com>
2023-02-24 13:04:27 -0300

    Qualification modify on CSS

--- jsa-eriques-personal-page/assets/js/main.js
+++ jsa-eriques-personal-page/assets/js/main.js
@@ -53,11 +53,26 @@ const skillsContent = document.getElementsByClassName('skills__content'),
 
     skillsHeader.forEach((el) =>{
         el.addEventListener('click', toggleSkills)
-    }
+    })
 
 /*==================== QUALIFICATION TABS ====================*/
 
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
 
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
+
+
+ 
 /*==================== SERVICES MODAL ====================*/
 
 
